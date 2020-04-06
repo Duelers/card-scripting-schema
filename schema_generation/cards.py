cd1c113c66408cfe397070b3a30d7d389b3aab53
Toren Darby <darby@science.ubc.ca>
2020-04-06 12:02:07 -0700

    Rewrite cards.py to match card models in cardboard architecture repo. Will be merged.

--- schema_generation/cards.py
+++ schema_generation/cards.py
@@ -1,5 +1,7 @@
 import typing
 
+import pydantic
+
 import card_types
 import factions
 import base
@@ -9,14 +11,29 @@ import effects as _effects
 
 class CardModel(base.BaseModel):
     name: str
-    cost: int
     faction: factions.Faction = factions.neutral
 
 
-class MinionCard(CardModel):
-    type: typing.Literal[card_types.minion]
-    attack: int
-    max_health: int
+class Castable(pydantic.BaseModel):
+    cost: pydantic.conint(ge=0)
+    copy_id: typing.Optional[int] = None # id added when deck is created such that no card with the same name in a
+    # deck has the same id
+
+class Unit(CardModel):
+    attack: pydantic.conint(ge=0)
+    max_health: pydantic.conint(ge=0)
+
+    current_health: typing.Optional[int] = None
+    # Can technically be negative at times. Relevant when taking damage and then healing in response.
+    # None while off the board. Set to max_health when etb.
+
+
+class GeneralCard(Unit):
+    type: typing.Literal[card_types.general] = card_types.general
+
+
+class MinionCard(Unit, Castable):
+    type: typing.Literal[card_types.minion] = card_types.minion
     tribes: typing.List[str] = []
     abilities: typing.Union[
         _abilities.Ability,
@@ -24,16 +41,16 @@ class MinionCard(CardModel):
     ]
 
 
-class SpellCard(CardModel):
-    type: typing.Literal[card_types.spell]
+class SpellCard(CardModel, Castable):
+    type: typing.Literal[card_types.spell] = card_types.spell
     effects: typing.Union[
         _effects.Effect,
         typing.List[_effects.Effect]
     ]
 
 
-class ArtifactCard(CardModel):
-    type: typing.Literal[card_types.artifact]
+class ArtifactCard(CardModel, Castable):
+    type: typing.Literal[card_types.artifact] = card_types.artifact
     abilities: typing.Union[
         _abilities.Ability,
         typing.List[_abilities.Ability]
