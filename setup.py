03f0f68c44f85c4a9923bfe0c563481ca98e277f
Toren Darby <darby@science.ubc.ca>
2020-04-06 14:02:27 -0700

    Trying to get setup.py to build properly.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='cardboard_card_scripting',
-    version='0.1.2',
+    version='0.1.3',
     packages=[''],
-    package_dir={'card_model': 'schema_generation'},
+    # package_dir={'card_model': 'schema_generation'},
     url='https://github.com/Duelers/card-scripting-schema',
     license='GNU GPLv3',
     author='Toren Darby',
     author_email='',
-    description='A card scripting schema for project Cardboard'
+    description='A card scripting schema for project Cardboard',
+    python_requires='>=3.8'
 )
