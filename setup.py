0b6b3ddc31286b2b35bb0f3c83462ea97ae661d1
Toren Darby <darby@science.ubc.ca>
2020-04-06 13:50:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='cardboard_card_scripting',
-    version='0.1.1',
+    version='0.1.2',
     packages=[''],
-    package_dir={'': 'schema_generation'},
+    package_dir={'card_model': 'schema_generation'},
     url='https://github.com/Duelers/card-scripting-schema',
-    license=' GNU GPLv3',
+    license='GNU GPLv3',
     author='Toren Darby',
     author_email='',
     description='A card scripting schema for project Cardboard'
